764b958409fcbad6b78c472c8d91c3d7864612e8
pch8388 <pch838811@gmail.com>
2019-10-18 00:04:07 +0900

    [feat] :  review controller 추가 / 테스트 추가

---
+++ eatgo-api/src/main/java/me/study/eatgo/application/ReviewService.java
@@ -0,0 +1,12 @@
+package me.study.eatgo.application;
+
+import me.study.eatgo.domain.Review;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    public void addReview(Review review) {
+        // TODO:
+    }
+}
