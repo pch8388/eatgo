c088d6a2acd379dc1fae86f78f4f793bb30cccfd
pch8388 <pch838811@gmail.com>
2019-10-21 23:37:52 +0900

    [feat] :  service addReview 구현

--- eatgo-api/src/test/java/me/study/eatgo/application/ReviewServiceTests.java
+++ eatgo-api/src/test/java/me/study/eatgo/application/ReviewServiceTests.java
@@ -1,16 +1,26 @@
 package me.study.eatgo.application;
 
 import me.study.eatgo.domain.Review;
+import me.study.eatgo.domain.ReviewRepository;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 
 public class ReviewServiceTests {
 
     private ReviewService reviewService;
 
+    @Mock
+    private ReviewRepository reviewRepository;
+
     @Before
     public void setUp() {
-        reviewService = new ReviewService();
+        MockitoAnnotations.initMocks(this);
+        reviewService = new ReviewService(reviewRepository);
     }
 
     @Test
@@ -22,5 +32,7 @@ public class ReviewServiceTests {
                 .build();
 
         reviewService.addReview(review);
+
+        verify(reviewRepository).save(any());
     }
 }
