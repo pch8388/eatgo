87a5ffc8490804c443ec374f5ca371cca39d6f59
pch8388 <pch838811@gmail.com>
2019-10-28 01:05:58 +0900

    [refactor] : 프로젝트 분리

---
+++ eatgo-customer-api/src/main/java/me/study/eatgo/interfaces/WelcomeController.java
@@ -0,0 +1,13 @@
+package me.study.eatgo.interfaces;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, world!!!";
+    }
+}
