9b753520b3b12e890154ca1b540c7249fa2bdad4
pch8388 <pch838811@gmail.com>
2019-10-29 00:25:43 +0900

    [feat] : 관리자가 review 전체를 볼 수 있게 추

--- eatgo-admin-api/src/main/java/me/study/eatgo/interfaces/ReviewController.java
+++ eatgo-admin-api/src/main/java/me/study/eatgo/interfaces/ReviewController.java
@@ -4,30 +4,26 @@ import me.study.eatgo.application.ReviewService;
 import me.study.eatgo.domain.Review;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 
 @RestController
 public class ReviewController {
 
+    private final ReviewService reviewService;
+
     @Autowired
-    private ReviewService reviewService;
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
 
-    @PostMapping("/restaurants/{restaurantId}/reviews")
-    public ResponseEntity<?> create(
-        @PathVariable("restaurantId") Long restaurantId,
-        @RequestBody @Valid Review resource) throws URISyntaxException {
-
-        Review review = reviewService.addReview(resource);
-        String url = "/restaurants/" + restaurantId +
-            "/reviews/" + review.getId();
-        return ResponseEntity.created(new URI(url))
-            .body("{}");
+    @GetMapping("/reviews")
+    public List<Review> list() {
+        List<Review> reviews = reviewService.getReviews();
+        return reviews;
     }
 }
