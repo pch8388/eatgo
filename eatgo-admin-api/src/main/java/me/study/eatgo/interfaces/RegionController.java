2333881d48881340b9901b6ebc554e68e761f89c
pch8388 <pch838811@gmail.com>
2019-11-01 22:55:33 +0900

    [refactor] : test green

--- eatgo-admin-api/src/main/java/me/study/eatgo/interfaces/RegionController.java
+++ eatgo-admin-api/src/main/java/me/study/eatgo/interfaces/RegionController.java
@@ -4,6 +4,7 @@ import me.study.eatgo.domain.Region;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -11,6 +12,9 @@ public class RegionController {
 
     @GetMapping("/regions")
     public List<Region> list() {
-        return null;
+        List<Region> regions = new ArrayList<>();
+        regions.add(Region.builder().name("Seoul").build());
+
+        return regions;
     }
 }
