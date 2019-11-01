900de22b4d6582770723ac0c874ebc5113385160
pch8388 <pch838811@gmail.com>
2019-11-01 21:09:33 +0900

    [feat] : Region 추가

---
+++ eatgo-common/src/main/java/me/study/eatgo/domain/Region.java
@@ -0,0 +1,23 @@
+package me.study.eatgo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Region {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+}
