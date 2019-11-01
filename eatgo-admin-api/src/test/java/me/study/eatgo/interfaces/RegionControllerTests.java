af6c505ca3e8a8901648b94de35ce4fecf6aee3d
pch8388 <pch838811@gmail.com>
2019-11-01 22:03:10 +0900

    [refactor] : test 추가

--- eatgo-admin-api/src/test/java/me/study/eatgo/interfaces/RegionControllerTests.java
+++ eatgo-admin-api/src/test/java/me/study/eatgo/interfaces/RegionControllerTests.java
@@ -7,7 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.core.StringContains.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -20,6 +22,7 @@ public class RegionControllerTests {
     @Test
     public void list() throws Exception {
         mvc.perform(get("/regions"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Seoul")));
     }
 }
