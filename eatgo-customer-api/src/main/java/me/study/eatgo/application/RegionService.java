d8c8fcff0ee5c119a8edb01e80e3dd98a9a4edbe
pch8388 <pch838811@gmail.com>
2019-11-03 22:16:00 +0900

    [refactor] : customer api 에서 사용할 로직들 복사

---
+++ eatgo-customer-api/src/main/java/me/study/eatgo/application/RegionService.java
@@ -0,0 +1,23 @@
+package me.study.eatgo.application;
+
+import me.study.eatgo.domain.Region;
+import me.study.eatgo.domain.RegionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RegionService {
+
+    private RegionRepository regionRepository;
+
+    @Autowired
+    public RegionService(RegionRepository regionRepository) {
+        this.regionRepository = regionRepository;
+    }
+
+    public List<Region> getRegions() {
+        return regionRepository.findAll();
+    }
+}
