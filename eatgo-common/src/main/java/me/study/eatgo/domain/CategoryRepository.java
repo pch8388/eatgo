0dac843ca9eded0ccde1c6b24e3997eeee7c4e0b
pch8388 <pch838811@gmail.com>
2019-11-03 22:59:45 +0900

    [feat] : category repository 추가

---
+++ eatgo-common/src/main/java/me/study/eatgo/domain/CategoryRepository.java
@@ -0,0 +1,13 @@
+package me.study.eatgo.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    List<Category> findAll();
+
+    Category save(Category category);
+
+}
