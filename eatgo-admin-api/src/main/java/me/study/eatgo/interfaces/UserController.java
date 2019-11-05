283e8155b3dcc073848cb92095e513d31885ffa9
pch8388 <pch838811@gmail.com>
2019-11-05 23:32:14 +0900

    [refactor] : test green

--- eatgo-admin-api/src/main/java/me/study/eatgo/interfaces/UserController.java
+++ eatgo-admin-api/src/main/java/me/study/eatgo/interfaces/UserController.java
@@ -1,6 +1,16 @@
 package me.study.eatgo.interfaces;
 
+import me.study.eatgo.domain.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class UserController {
 
-
+    @GetMapping("/users")
+    public List<User> list() {
+        return null;
+    }
 }
