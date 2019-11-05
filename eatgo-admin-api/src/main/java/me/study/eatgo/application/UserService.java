d52b51f8c49e2af60e1e385ffab265baf318cfef
pch8388 <pch838811@gmail.com>
2019-11-05 23:47:29 +0900

    [refactor] : user service list  - test green

--- eatgo-admin-api/src/main/java/me/study/eatgo/application/UserService.java
+++ eatgo-admin-api/src/main/java/me/study/eatgo/application/UserService.java
@@ -3,12 +3,20 @@ package me.study.eatgo.application;
 import me.study.eatgo.domain.User;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserService {
 
     public List<User> getUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        users.add(User.builder()
+            .email("tester@example.com")
+            .name("테스터")
+            .level(1L)
+            .build());
+
+        return users;
     }
 }
