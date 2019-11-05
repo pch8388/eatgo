0e56696708d87d6bf4d134e26b5a57eb872ad809
pch8388 <pch838811@gmail.com>
2019-11-06 00:03:58 +0900

    [refactor] : user create - test green

--- eatgo-admin-api/src/main/java/me/study/eatgo/interfaces/UserController.java
+++ eatgo-admin-api/src/main/java/me/study/eatgo/interfaces/UserController.java
@@ -3,9 +3,13 @@ package me.study.eatgo.interfaces;
 import me.study.eatgo.application.UserService;
 import me.study.eatgo.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -17,5 +21,11 @@ public class UserController {
     @GetMapping("/users")
     public List<User> list() {
         return userService.getUsers();
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<?> create() throws URISyntaxException {
+        String url = "/users/1";
+        return ResponseEntity.created(new URI(url)).body("{}");
     }
 }
