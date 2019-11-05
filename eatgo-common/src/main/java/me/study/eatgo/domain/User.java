219f11eeff43729c155e0d73c9e3ba02f8ba9d68
pch8388 <pch838811@gmail.com>
2019-11-05 23:35:13 +0900

    [test] : user 생성 테스트 생성 - test red

--- eatgo-common/src/main/java/me/study/eatgo/domain/User.java
+++ eatgo-common/src/main/java/me/study/eatgo/domain/User.java
@@ -1,4 +1,33 @@
 package me.study.eatgo.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    private String name;
+
+    @NotNull
+    private Long level;
 }
---
+++ eatgo-common/src/test/java/me/study/eatgo/domain/UserTests.java
@@ -0,0 +1,20 @@
+package me.study.eatgo.domain;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class UserTests {
+
+    @Test
+    public void creation() {
+        User user = User.builder()
+            .email("tester@example.com")
+            .name("테스터")
+            .level(1L)
+            .build();
+
+        assertThat(user.getName(), is("테스트"));
+    }
+}
