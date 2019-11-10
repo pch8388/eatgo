89e4fa195121e210529553f5bf16b79a87c56fcd
pch8388 <pch838811@gmail.com>
2019-11-10 23:39:04 +0900

    [refactor] : user 가 실제로 생성되도록 수정

--- eatgo-customer-api/src/main/java/me/study/eatgo/interfaces/UserController.java
+++ eatgo-customer-api/src/main/java/me/study/eatgo/interfaces/UserController.java
@@ -1,9 +1,11 @@
 package me.study.eatgo.interfaces;
 
+import me.study.eatgo.domain.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotEmpty;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -12,6 +14,16 @@ public class UserController {
 
     @PostMapping("/users")
     public ResponseEntity<?> create() throws URISyntaxException {
+        String email = "tester@example.com";
+        String name = "Tester";
+        String password = "test";
+
+        User user = User.builder()
+                .email(email)
+                .name(name)
+                .password(password)
+                .build();
+
         String url = "/users/1004";
         return ResponseEntity.created(new URI(url)).body("{}");
     }
