e62f899e783ed2c17d8a38095821a91f071cd224
pch8388 <pch838811@gmail.com>
2019-11-10 23:49:42 +0900

    [refactor] : controller 에서 user service 를 실제로 실행하도록 수정

--- eatgo-customer-api/src/main/java/me/study/eatgo/interfaces/UserController.java
+++ eatgo-customer-api/src/main/java/me/study/eatgo/interfaces/UserController.java
@@ -1,6 +1,7 @@
 package me.study.eatgo.interfaces;
 
 import me.study.eatgo.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,18 +12,23 @@ import java.net.URISyntaxException;
 @RestController
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/users")
     public ResponseEntity<?> create() throws URISyntaxException {
         String email = "tester@example.com";
         String name = "Tester";
         String password = "test";
 
-        User user = User.builder()
-                .id(1004L)
-                .email(email)
-                .name(name)
-                .password(password)
-                .build();
+//        User user = User.builder()
+//                .id(1004L)
+//                .email(email)
+//                .name(name)
+//                .password(password)
+//                .build();
+
+        User user = userService.registerUser(email, name, password);
 
         String url = "/users/1004";
         return ResponseEntity.created(new URI(url)).body("{}");
