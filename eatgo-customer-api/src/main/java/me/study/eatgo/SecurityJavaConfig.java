93835173e53eb9e86a87abe7d5a385f6900a54c9
pch8388 <pch838811@gmail.com>
2019-11-13 23:06:24 +0900

    [refactor] : PasswordEncoder bean 설정

--- eatgo-customer-api/src/main/java/me/study/eatgo/SecurityJavaConfig.java
+++ eatgo-customer-api/src/main/java/me/study/eatgo/SecurityJavaConfig.java
@@ -1,10 +1,13 @@
 package me.study.eatgo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -21,5 +24,10 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
             //.addFilter(filter)
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
