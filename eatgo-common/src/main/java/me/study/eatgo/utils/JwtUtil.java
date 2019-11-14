f02d70c53c8b2c672a34a98179820a0f65314a2d
pch8388 <pch838811@gmail.com>
2019-11-15 00:00:40 +0900

    [feat] : jjwt 사용

--- eatgo-common/src/main/java/me/study/eatgo/utils/JwtUtil.java
+++ eatgo-common/src/main/java/me/study/eatgo/utils/JwtUtil.java
@@ -1,11 +1,15 @@
 package me.study.eatgo.utils;
 
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtil {
 
-    public String createToken(Long id, String name) {
-        return "header.payload.signature";
+    public String createToken(Long userId, String userName) {
+        String token = Jwts.builder()
+            .claim("userId", userId)
+            .compact();
+        return token;
     }
 }
