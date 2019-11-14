a7f559bae507867a0c6ddec7a45bc5f2c2ac92fe
pch8388 <pch838811@gmail.com>
2019-11-15 00:18:33 +0900

    [refactor] : Key 를 객체생성시에 만들도록 변경

--- eatgo-common/src/main/java/me/study/eatgo/utils/JwtUtil.java
+++ eatgo-common/src/main/java/me/study/eatgo/utils/JwtUtil.java
@@ -3,18 +3,18 @@ package me.study.eatgo.utils;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.stereotype.Component;
 
 import java.security.Key;
 
-@Component
 public class JwtUtil {
 
+    private final Key key;
+
+    public JwtUtil(String secret) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
     public String createToken(Long userId, String name) {
-
-        String secret = "12345678901234567890123456789012";
-        Key key = Keys.hmacShaKeyFor(secret.getBytes());
-
         String token = Jwts.builder()
             .claim("userId", userId)
             .claim("name", name)
