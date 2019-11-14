586d38928b161a1bc188f414f7c7788ea02c0f2f
pch8388 <pch838811@gmail.com>
2019-11-14 23:54:58 +0900

    [refactor] : jwtUtil test 내용 추가

--- eatgo-customer-api/src/main/java/me/study/eatgo/interfaces/SessionController.java
+++ eatgo-customer-api/src/main/java/me/study/eatgo/interfaces/SessionController.java
@@ -3,7 +3,6 @@ package me.study.eatgo.interfaces;
 import me.study.eatgo.application.UserService;
 import me.study.eatgo.domain.User;
 import me.study.eatgo.utils.JwtUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
