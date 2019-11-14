a1caf82fc7d8b1b50ae2bd379f1c6dc63b33978d
pch8388 <pch838811@gmail.com>
2019-11-15 00:04:26 +0900

    [refactor] : sign 추가

--- eatgo-common/src/main/java/me/study/eatgo/utils/JwtUtil.java
+++ eatgo-common/src/main/java/me/study/eatgo/utils/JwtUtil.java
@@ -1,14 +1,24 @@
 package me.study.eatgo.utils;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
+
+import java.security.Key;
 
 @Component
 public class JwtUtil {
 
-    public String createToken(Long userId, String userName) {
+    public String createToken(Long userId, String name) {
+
+        String secret = "12345678901234567890123456789012";
+        Key key = Keys.hmacShaKeyFor(secret.getBytes());
+
         String token = Jwts.builder()
             .claim("userId", userId)
+            .claim("name", name)
+            .signWith(key, SignatureAlgorithm.HS256)
             .compact();
         return token;
     }
