bf6f408b6e0dc4851e270ee203cde7ee1606d672
pch8388 <pch838811@gmail.com>
2019-11-21 00:01:14 +0900

    [refactor] : test green

--- eatgo-customer-api/src/main/java/me/study/eatgo/interfaces/ReservationController.java
+++ eatgo-customer-api/src/main/java/me/study/eatgo/interfaces/ReservationController.java
@@ -1,5 +1,6 @@
 package me.study.eatgo.interfaces;
 
+import me.study.eatgo.application.ReservationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,10 +12,24 @@ import java.net.URISyntaxException;
 @RestController
 public class ReservationController {
 
+    private ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
     @PostMapping("/restaurants/{restaurantId}/reservations")
     public ResponseEntity<?> create(
         @PathVariable Long restaurantId
     ) throws URISyntaxException {
+        Long userId = 1L;
+        String name = "Tester";
+        String date = "2019-12-24";
+        String time = "20:00";
+        Integer partySize = 20;
+
+        reservationService.addReservation(userId, name, date, time, partySize);
+
         String url = "/restaurants/"+restaurantId+"/reservations/1";
         return ResponseEntity.created(new URI(url)).body("{}");
     }
