5484f357c8fcd3547fa2543021f36ae0b100e400
pch8388 <pch838811@gmail.com>
2019-11-20 23:59:55 +0900

    [test] : service 를 호출 하는 test 추가 - test red

--- eatgo-customer-api/src/test/java/me/study/eatgo/interfaces/ReservationControllerTests.java
+++ eatgo-customer-api/src/test/java/me/study/eatgo/interfaces/ReservationControllerTests.java
@@ -1,12 +1,16 @@
 package me.study.eatgo.interfaces;
 
+import me.study.eatgo.application.ReservationService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -17,9 +21,21 @@ public class ReservationControllerTests {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private ReservationService reservationService;
+
     @Test
     public void create() throws Exception {
+        Long userId = 1L;
+        String name = "Tester";
+        String date = "2019-12-24";
+        String time = "20:00";
+        Integer partySize = 20;
+
         mvc.perform(post("/restaurants/1004/reservations"))
                 .andExpect(status().isCreated());
+
+        verify(reservationService).addReservation(
+                eq(userId), eq(name), eq(date), eq(time), eq(partySize));
     }
 }
