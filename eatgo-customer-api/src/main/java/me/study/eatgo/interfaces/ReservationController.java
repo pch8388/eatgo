be5d08e013eeadb0d2d1ec7d0c8f5bc3568c25cc
pch8388 <pch838811@gmail.com>
2019-11-21 00:18:02 +0900

    [refactor] : user 정보를 jwt token 으로부터 받아오도록 변경

--- eatgo-customer-api/src/main/java/me/study/eatgo/interfaces/ReservationController.java
+++ eatgo-customer-api/src/main/java/me/study/eatgo/interfaces/ReservationController.java
@@ -1,9 +1,13 @@
 package me.study.eatgo.interfaces;
 
+import io.jsonwebtoken.Claims;
 import me.study.eatgo.application.ReservationService;
+import me.study.eatgo.domain.Reservation;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -20,15 +24,21 @@ public class ReservationController {
 
     @PostMapping("/restaurants/{restaurantId}/reservations")
     public ResponseEntity<?> create(
-        @PathVariable Long restaurantId
+        Authentication authentication,
+        @PathVariable Long restaurantId,
+        @RequestBody Reservation resource
     ) throws URISyntaxException {
-        Long userId = 1L;
-        String name = "Tester";
-        String date = "2019-12-24";
-        String time = "20:00";
-        Integer partySize = 20;
 
-        reservationService.addReservation(userId, name, date, time, partySize);
+        Claims claims = (Claims) authentication.getPrincipal();
+
+        Long userId = claims.get("userId", Long.class);
+        String name = claims.get("name", String.class);
+        String date = resource.getDate();
+        String time = resource.getTime();
+        Integer partySize = resource.getPartySize();
+
+        reservationService.addReservation(
+            restaurantId, userId, name, date, time, partySize);
 
         String url = "/restaurants/"+restaurantId+"/reservations/1";
         return ResponseEntity.created(new URI(url)).body("{}");
