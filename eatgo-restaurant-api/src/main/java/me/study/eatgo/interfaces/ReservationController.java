e26942500303f8635c3eb5ef587d641b27181993
pch8388 <pch838811@gmail.com>
2019-11-22 00:53:47 +0900

    [feat] : list 구현

--- eatgo-restaurant-api/src/main/java/me/study/eatgo/interfaces/ReservationController.java
+++ eatgo-restaurant-api/src/main/java/me/study/eatgo/interfaces/ReservationController.java
@@ -3,13 +3,10 @@ package me.study.eatgo.interfaces;
 import io.jsonwebtoken.Claims;
 import me.study.eatgo.application.ReservationService;
 import me.study.eatgo.domain.Reservation;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.util.List;
 
 @RestController
 public class ReservationController {
@@ -21,7 +18,15 @@ public class ReservationController {
     }
 
     @GetMapping("/restaurants")
-    public String list() {
-        return "";
+    public List<Reservation> list(
+        Authentication authentication
+    ) {
+        Claims claims = (Claims) authentication.getPrincipal();
+        Long restaurantId = claims.get("restaurantId", Long.class);
+
+        List<Reservation> reservations =
+            reservationService.getReservations(restaurantId);
+
+        return reservations;
     }
 }
--- eatgo-restaurant-api/src/test/java/me/study/eatgo/interfaces/ReservationControllerTests.java
+++ eatgo-restaurant-api/src/test/java/me/study/eatgo/interfaces/ReservationControllerTests.java
@@ -1,22 +1,16 @@
 package me.study.eatgo.interfaces;
 
 import me.study.eatgo.application.ReservationService;
-import me.study.eatgo.domain.Reservation;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -31,10 +25,12 @@ public class ReservationControllerTests {
 
     @Test
     public void list() throws Exception {
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEwMDQsIm5hbWUiOiJKb2huIn0.8hm6ZOJykSINHxL-rf0yV882fApL3hyQ9-WGlJUyo2A";
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjIwMjAsIm5hbWUiOiJPd25lciIsInJlc3RhdXJhbnRJZCI6MTAwNH0.a5n4PWJ2-3yVyMaLGG0HSPXtH_mgpOvofpQ1OFkgDOQ";
 
         mvc.perform(get("/restaurants")
             .header("Authorization", "Bearer " + token))
             .andExpect(status().isOk());
+
+        verify(reservationService).getReservations(1004L);
     }
 }
